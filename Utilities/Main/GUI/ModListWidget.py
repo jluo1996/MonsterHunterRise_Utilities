5832db821efc971e354e84c99aff9e8d1fda62c0
Jianliang Luo <jayluo1996@gmail.com>
2026-01-15 14:56:13 -0800

    Add select all button

--- Utilities/Main/GUI/ModListWidget.py
+++ Utilities/Main/GUI/ModListWidget.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QScrollArea
+from PyQt6.QtWidgets import QHBoxLayout, QWidget, QVBoxLayout, QScrollArea, QCheckBox
+from PyQt6.QtCore import Qt
 from Main.GUI.ModItemWidget import ModItemWidget
 from Main.Mods.ModModel import ModModel
 
@@ -8,6 +9,9 @@ class ModListWidget(QWidget):
         super().__init__(parent)
 
         main_layout = QVBoxLayout(self)
+
+        # Add "Select All" checkbox
+        
 
         scroll = QScrollArea()
         scroll.setWidgetResizable(True)
@@ -19,6 +23,14 @@ class ModListWidget(QWidget):
         layout = QVBoxLayout(container)
         layout.setSpacing(4)
 
+        checkbox_container = QWidget()
+        container_layout = QHBoxLayout(checkbox_container)
+        container_layout.setContentsMargins(5, 0, 0, 0)  # left, top, right, bottom
+        self.select_all_checkbox = QCheckBox("Select All")
+        self.select_all_checkbox.stateChanged.connect(self.on_select_all_changed)
+        container_layout.addWidget(self.select_all_checkbox)
+        layout.addWidget(checkbox_container)
+
         self.mods_item_widgets = []
         for mod in mods:
             item = ModItemWidget(mod)
@@ -26,6 +38,11 @@ class ModListWidget(QWidget):
             self.mods_item_widgets.append(item)
 
         layout.addStretch()
+
+    def on_select_all_changed(self, state):
+        checked = state == Qt.CheckState.Checked.value
+        for item_widget in self.mods_item_widgets:
+            item_widget.check_box.setChecked(checked)
 
     def refresh_statuses(self):
         for item_widget in self.mods_item_widgets:
