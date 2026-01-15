f2ec91fbc2a4caf92346f22d50dca0fe43ac2904
Jianliang Luo <jayluo1996@gmail.com>
2026-01-15 14:35:06 -0800

    Add auto cohoot nest mod

---
+++ Utilities/Main/Mods/AutoCohootNestMod.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from Main.Mods.ModModel import ModModel
+
+class AutoCohootNestMod(ModModel):
+    def __init__(self, resources_path, game_install_path):
+        super().__init__(resources_path, game_install_path)
+        self.name = "Auto Cohoot Nest"
+        self.description = "Automatically collects items from Cohoot nests."
+        self.mod_file_path = Path(self.resource_folder) / "Auto Cohoot Nest"
+        self.update_install_path(game_install_path)
+
+    def update_install_path(self, new_game_install_path):
+        self.install_path = Path(new_game_install_path) / "reframework" / "autorun"
