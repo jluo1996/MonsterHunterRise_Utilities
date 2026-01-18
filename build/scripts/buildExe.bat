fbc8d494c62930fa037e10d8353d1fadfedadf98
Jianliang Luo <jayluo1996@gmail.com>
2026-01-17 19:46:14 -0800

    Update build script

--- build/scripts/buildExe.bat
+++ build/scripts/buildExe.bat
@@ -1,7 +1,17 @@
 @echo off
 
-set "appName=%~1"
+cd /d "%~dp0..\.."
+
+set "appName=%~1" 
+set "versionMajor=%~2"
+set "versionMinor=%~3"
+set "versionBuild=%~4"
+set "versionRevision=%~5"
+set "versionFile=%~dp0..\version_info.txt"
+
+echo Writing version info ...
+python314\python.exe "build\scripts\prebuild.py" "%appName%" "%versionMajor%" "%versionMinor%" "%versionBuild%" "%versionRevision%" %versionFile%
 
 echo Generating "%appName%.exe" in "%~f0" ...
-
-python314\python.exe -m PyInstaller --onefile --noconsole --add-data "Utilities\Resources;\Resources" --name="%appName%" "Utilities\MainPy.py"
+ 
+python314\python.exe -m PyInstaller --onefile --noconsole --version-file "build\version_info.txt" --add-data "Utilities\Resources;\Resources" --name="%appName%" "Utilities\MainPy.py"
---
+++ build/scripts/prebuild.py
@@ -0,0 +1,73 @@
+import sys
+import re
+from pathlib import Path
+
+if len(sys.argv) != 7:
+    print("Usage: python update_version.py <app_name> <version_major> <version_minor> <version_build> <version_revision> <version_file>")
+    sys.exit(1)
+
+app_name = sys.argv[1]
+major = sys.argv[2]
+minor = sys.argv[3]
+build = sys.argv[4]
+revision = sys.argv[5]
+version_file = Path(sys.argv[6])
+
+version = f"{major}.{minor}.{build}.{revision}"
+tuple_version = f"({major}, {minor}, {build}, {revision})"
+
+text = version_file.read_text(encoding="utf-8")
+
+# ================ Update version info in the file ================
+# --- Replace numeric versions ---
+text = re.sub(
+    r"filevers=\([^)]+\)",
+    f"filevers={tuple_version}",
+    text
+)
+
+text = re.sub(
+    r"prodvers=\([^)]+\)",
+    f"prodvers={tuple_version}",
+    text
+)
+
+# --- Replace string versions ---
+text = re.sub(
+    r"StringStruct\('FileVersion',\s*'[^']*'\)",
+    f"StringStruct('FileVersion', '{version}')",
+    text
+)
+
+text = re.sub(
+    r"StringStruct\('ProductVersion',\s*'[^']*'\)",
+    f"StringStruct('ProductVersion', '{version}')",
+    text
+)
+
+print(f"Updated version to {version}")
+# ================================================================
+
+
+# ================ Update all names in the file ================
+# --- Replace product name using app_name ---
+text = re.sub(
+    r"StringStruct\('ProductName',\s*'[^']*'\)",
+    f"StringStruct('ProductName', '{app_name}')",
+    text
+)
+text = re.sub(
+    r"StringStruct\('InternalName',\s*'[^']*'\)",
+    f"StringStruct('InternalName', '{app_name}')",
+    text
+)
+text = re.sub(
+    r"StringStruct\('OriginalFilename',\s*'[^']*'\)",
+    f"StringStruct('OriginalFilename', '{app_name}.exe')",
+    text
+)
+print(f"Updated names to {app_name}")
+# ================================================================
+
+version_file.write_text(text, encoding="utf-8")
+print(f"Updated file: {version_file.resolve()}")
