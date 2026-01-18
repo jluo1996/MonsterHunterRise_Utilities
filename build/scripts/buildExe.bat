9755d0c2833e6645c1b03f1aca619e1e3eae68a4
Jianliang Luo <jayluo1996@gmail.com>
2026-01-17 21:28:32 -0800

    update build script

--- build/scripts/buildExe.bat
+++ build/scripts/buildExe.bat
@@ -3,24 +3,11 @@
 cd /d "%~dp0..\.."
 
 set "appName=%~1" 
-set "versionMajor=%~2"
-set "versionMinor=%~3"
-set "versionBuild=%~4"
-set "versionRevision=%~5"
-set "versionFile=%~dp0..\version_info.txt"
-
-@REM echo Writing version info ...
-@REM python314\python.exe "build\scripts\prebuild.py" "%appName%" "%versionMajor%" "%versionMinor%" "%versionBuild%" "%versionRevision%" %versionFile%
-
-@REM REM ---- Stop if prebuild failed ----
-@REM if errorlevel 1 (
-@REM     echo ERROR: prebuild.py failed. Aborting build.
-@REM     exit /b 1
-@REM )
+set "pythonPath=%~2"
 
 echo Generating "%appName%.exe" in "%~f0" ...
- 
-python314\python.exe -m PyInstaller --onefile --noconsole --version-file "build\version_info.txt" --add-data "Utilities\Resources;\Resources" --name="%appName%" "Utilities\MainPy.py"
+
+%pythonPath% -m PyInstaller --onefile --noconsole --version-file "build\version_info.txt" --add-data "Utilities\Resources;\Resources" --name="%appName%" "Utilities\MainPy.py"
 
 REM ---- Stop if PyInstaller failed ----
 if errorlevel 1 (
