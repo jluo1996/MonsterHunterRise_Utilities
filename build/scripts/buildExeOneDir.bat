9e51c5850541780d3265e2bdb3e6bb666ebbc579
Jianliang Luo <jayluo1996@gmail.com>
2026-01-20 15:50:44 -0800

    Create one dir exe as well

---
+++ build/scripts/buildExeOneDir.bat
@@ -0,0 +1,20 @@
+@echo off
+
+cd /d "%~dp0..\.."
+
+set "appName=%~1" 
+set "pythonPath=%~2"
+set "versionFile=%~3"
+
+echo Generating "%appName%.exe" in "%~f0" ...
+
+%pythonPath% -m PyInstaller --onedir --clean --noconfirm --noconsole --version-file "%versionFile%" --add-data "Utilities\Resources;Resources" --name="%appName%" "Utilities\MainPy.py"
+
+REM ---- Stop if PyInstaller failed ----
+if errorlevel 1 (
+    echo ERROR: PyInstaller failed.
+    exit /b 1
+)
+
+echo Build completed successfully.
+exit /b 0
