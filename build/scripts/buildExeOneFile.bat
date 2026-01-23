7cf7f192ae76187162d83bc9bfd5e46c758817db
Jianliang Luo <jayluo1996@gmail.com>
2026-01-22 23:16:18 -0800

    Remove python environment from repo and set it up during action

--- build/scripts/buildExeOneFile.bat
+++ build/scripts/buildExeOneFile.bat
@@ -3,12 +3,11 @@
 cd /d "%~dp0..\.."
 
 set "appName=%~1" 
-set "pythonPath=%~2"
-set "versionFile=%~3"
+set "versionFile=%~2"
 
 echo Generating "%appName%.exe" in "%~f0" ...
 
-%pythonPath% -m PyInstaller --onefile --clean --noconfirm --noconsole --version-file "%versionFile%" --add-data "Utilities\Resources;Resources" --name="%appName%" "Utilities\MainPy.py"
+python -m PyInstaller --onefile --clean --noconfirm --noconsole --version-file "%versionFile%" --add-data "Utilities\Resources;Resources" --name="%appName%" "Utilities\MainPy.py"
 
 REM ---- Stop if PyInstaller failed ----
 if errorlevel 1 (
