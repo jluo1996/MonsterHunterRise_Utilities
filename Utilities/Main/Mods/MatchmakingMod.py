56e044a510e894492a42c70b128ab7bb1feb71a7
Jianliang Luo <jayluo1996@gmail.com>
2026-02-16 18:05:14 -0800

    Update mod name

--- Utilities/Main/Mods/MatchmakingMod.py
+++ Utilities/Main/Mods/MatchmakingMod.py
@@ -4,7 +4,7 @@ from Main.Mods.REFrameWorkDependentMod import REFrameWorkDependentMod
 class MatchmakingMod(REFrameWorkDependentMod):
     def __init__(self, resource_folder: str, game_install_path: str, state_file_helper):
         super().__init__(resource_folder, game_install_path, state_file_helper)
-        self.name = "Matchmaking"
+        self.name = "Better Matchmaking"
         self.description = "Disables timeout when searching for Join Requests. Disables Region Lock for Join Requests and Lobbies. Fixes Language Filter for Lobbies."
         self.mod_file_path = Path(self.resource_folder) / "Better Matchmaking"
         self.update_install_path(game_install_path)
