84bf8e15785f9cf5ae74badf2f095c5e9c419dbd
Jianliang Luo <jayluo1996@gmail.com>
2026-02-17 15:28:09 -0800

    Fix syntax

--- build/scripts/buildExeOneDir.bat
+++ build/scripts/buildExeOneDir.bat
@@ -7,7 +7,7 @@ set "versionFile=%~2"
 
 echo Generating "%appName%.exe" in "%~f0" ...
 
-python -m PyInstaller -O --onedir --clean --noconfirm --noconsole --version-file "%versionFile%" --add-data "Utilities\Resources;Resources" --name="%appName%" "Utilities\MainPy.py"
+python -O -m PyInstaller --onedir --clean --noconfirm --noconsole --version-file "%versionFile%" --add-data "Utilities\Resources;Resources" --name="%appName%" "Utilities\MainPy.py"
 
 REM ---- Stop if PyInstaller failed ----
 if errorlevel 1 (
