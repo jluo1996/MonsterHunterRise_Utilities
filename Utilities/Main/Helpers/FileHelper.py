4ec19c69850872bfe4d70035a5441a65266436de
Jianliang Luo <jayluo1996@gmail.com>
2026-03-01 16:05:19 -0800

    Update file location

--- Utilities/Main/Helpers/FileHelper.py
+++ Utilities/Main/Helpers/FileHelper.py
@@ -16,12 +16,12 @@ class FileHelper:
         return self.get_resources_folder_path() / "Pictures"
     
     def get_log_folder_path(self):
-        log_folder = self._get_app_data_folder_path() / "MHR_Utilities_Logs"
+        log_folder = self.get_app_data_folder_path() / "MHR_Utilities_Logs"
         log_folder.mkdir(parents=True, exist_ok=True)
         return log_folder
     
     def get_install_state_file_path(self):
-        install_state_file_path = self._get_app_data_folder_path() / "mhr_utilities_install_state.json"
+        install_state_file_path = self.get_app_data_folder_path() / "mhr_utilities_install_state.json"
         try:
             with open(install_state_file_path, "x") as f:   # "x" = create only if not exist
                 f.write("{}")  # write default content
@@ -30,7 +30,7 @@ class FileHelper:
             self._log("Install state file already exists.", level="ERROR")
         return install_state_file_path
     
-    def _get_app_data_folder_path(self):
+    def get_app_data_folder_path(self):
         base_path = Path(os.getenv('LOCALAPPDATA'))
         app_data_folder_path = base_path / self.APP_NAME
         app_data_folder_path.mkdir(parents=True, exist_ok=True)
@@ -39,3 +39,8 @@ class FileHelper:
     def _log(self, message, level="INFO"):
         if self.logger:
             self.logger.log(message, level)
+
+    def get_app_data_temp_folder_path(self):
+        temp_folder = self.get_app_data_folder_path() / "temp"
+        temp_folder.mkdir(parents=True, exist_ok=True)
+        return temp_folder
